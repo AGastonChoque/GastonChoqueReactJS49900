b5d6f0886775c3001ebf8483f5c52285aa70ddfd
Gaston Choque <fgastonchoque95@gmail.com>
2023-11-21 18:25:54 -0300

    Navbar creada

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import classes from "./Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand text-light" href="#">Menú</a>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active text-light">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active text-light">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active text-light">Carrito</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Buscar"/>
+                        <button className="btn btn-outline-light" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
