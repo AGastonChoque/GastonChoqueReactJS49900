42bef59ef4f18446090c17586ca0a5c606bad7d7
Gaston Choque <fgastonchoque95@gmail.com>
2024-01-10 21:01:01 -0300

    eventos y item count sincronizado

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,15 +1,34 @@
 import classes from "./Item.module.css"
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 const Item = ({ id, name, pictureUrl, price }) => {
-    return (
 
-        <div className={classes.cardItemMan1}>
+    const [classBtn, setClassBtn] = useState("btn btn-secondary")
+    const divRef = useRef()
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            const classDiv = divRef.current
+            const {y} = classDiv.getBoundingClientRect()
+            const newClass = y < -70 ? "btn btn-outline-secondary" : "btn btn-secondary"
+            setClassBtn(newClass)
+        }
+        window.addEventListener("scroll", handleScroll)
+        
+        return() => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
+
+    return (
+        <div ref={divRef} className={classes.cardItemMan1}>
             <img src={pictureUrl} className={classes.styleImg} alt={name}/>
             <div>
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">${price.toFixed(2)}</p>
-                <Link to={`/detail/${id}`} className="btn btn-outline-secondary">Ver mas </Link>
+                <Link to={`/detail/${id}`} className={`${classBtn}`}>Ver mas </Link>
             </div>
         </div>
     )
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,19 @@
 import classes from "./ItemDetail.module.css"
 import ItemCount from "../ItemCount/ItemCount"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const ItemDetail = ({ id, name, price, category, pictureUrl, stock, description }) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const handleOnAdd = (count) => {
+        const objProductToAdd = {
+            id, name, price, category, pictureUrl
+        }
+        setQuantityAdded(count)
+    }
+
+
     return (
 
         <div className={classes.cardItemDetailMan1}>
@@ -13,7 +25,17 @@ const ItemDetail = ({ id, name, price, category, pictureUrl, stock, description 
                 <p className="card-text"><small>{category}</small></p>
                 <p className="card-text">{description}</p>
                 <p className="card-text">${price}</p>
-                <ItemCount initialValue={1} incrementBy={1} stock={stock} />
+                {
+                    quantityAdded > 0 ? (
+                        <div>
+                            <Link to="/" className="btn btn-warning m-1">Seguir comprando</Link>
+                            <Link to="/cart" className="btn btn-success m-1">Terminar Compra</Link>
+                        </div>
+                        
+                    ) : (
+                        <ItemCount initialValue={1} incrementBy={1} stock={stock} onAdd={handleOnAdd}/>
+                    )
+                }
             </div>
         </div>
     )
