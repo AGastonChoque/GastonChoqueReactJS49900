d20213cad0ee17a45cd47439dc11a232ae15a5b8
Gaston Choque <gastonchoque95@gmail.com>
2024-01-16 21:26:58 -0300

    Categorias por firebase

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { db } from "../../services/firebase/firebaseConfig"
+import { getDocs, collection, query, orderBy } from "firebase/firestore"
+import { useEffect, useState } from "react"
 
 const Navbar = () => {
+    const [categories, setCategories] = useState([])
+    const navigate = useNavigate ()
+
+    useEffect(() => {
+        const categoriesCollection = query(collection(db, "categories"), orderBy("order", "asc"))
+
+        getDocs(categoriesCollection)
+        .then(querySnapshot => {
+            const CategoriesAdapted = querySnapshot.docs.map(doc => {
+                const fields = doc.data()
+                return { id: doc.id, ...fields}
+            })
+
+            setCategories(CategoriesAdapted)
+        })
+    }, [])
+
+
     return (
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <p className="navbar-brand text-light">Menú</p>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link to="/category/celular" className="nav-link active text-light">Celular</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/category/tablet" className="nav-link active text-light">Tablet</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/category/notebook" className="nav-link active text-light">Notebook</Link>
-                            </li>
+                            {
+                                categories.map(cat => {
+                                    return <li className="nav-item">
+                                    <Link key={cat.id} to={`/category/${cat.slug}`} className="nav-link active text-light">{cat.name}</Link>
+                                </li>
+                                })
+                            }
                         </ul>
                         <div className="d-flex flex-column">
                             <form className="d-flex">
